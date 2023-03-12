006e4802a3ba7de3a33764ffced92ae905f91088
Huỳnh Phương Đại <phuongdai.saigon@gmail.com>
2023-03-12 13:01:47 +0700

    Question 2

---
+++ LA3_Q2/MyPoint.java
@@ -0,0 +1,27 @@
+package LA3_Q2;
+
+public class MyPoint {
+    private int x;
+    private int y;
+    public MyPoint(int inX,int inY)
+    {
+        inX = x;
+        inY = x;
+    }
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+    public int getX()
+    {
+        return x;
+    }
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+    public int getY()
+    {
+        return y;
+    }
+}
---
+++ LA3_Q2/MyTriangle.java
@@ -0,0 +1,11 @@
+package LA3_Q2;
+
+public class MyTriangle extends MyPoint
+{
+
+    public MyTriangle(int inX, int inY) 
+    {
+        super(inX, inY);
+    }
+    
+}
